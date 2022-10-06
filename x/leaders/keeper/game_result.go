0c77d53c2ad42f0fa23bd31c960162fa084ae7ad
Thomas Dekeyser <thomas@interchain.io>
2022-10-06 15:57:12 +0200

    add gamePacket onRecvPacket logic

--- x/leaders/keeper/game_result.go
+++ x/leaders/keeper/game_result.go
@@ -1,6 +1,8 @@
 package keeper
 
 import (
+	"strconv"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	channeltypes "github.com/cosmos/ibc-go/v3/modules/core/04-channel/types"
 	"github.com/tmsdkeys/leaders/x/leaders/types"
@@ -14,6 +16,27 @@ func (k Keeper) OnRecvGameResultPacket(ctx sdk.Context, packet channeltypes.Pack
 	}
 
 	// TODO: packet reception logic
+	topRank, found := k.GetTopRanked(ctx)
+	if !found {
+		k.NewLeader(ctx, data)
+	}
+	topScore, err := strconv.ParseUint(topRank.Score, 10, 64)
+	if err != nil {
+		return types.GameResultPacketAck{GameId: data.GameId}, err
+	}
+
+	if data.WinCount > topScore {
+		k.NewLeader(ctx, data)
+	}
 
 	return packetAck, nil
+}
+
+func (k Keeper) NewLeader(ctx sdk.Context, data types.GameResultPacketData) error {
+	newTopRank := types.TopRanked{
+		Address: data.Address,
+		Score:   strconv.FormatUint(data.WinCount, 10),
+	}
+	k.SetTopRanked(ctx, newTopRank)
+	return nil
 }
